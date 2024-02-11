ce25aa05ab1304bceacabe6887099e6f54ceb4f3
Zak Habul <112983162+ptewie@users.noreply.github.com>
2024-02-11 16:12:43 -0700

    Accidentally left over some unused interface code, fixed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-namespace Player
+namespace Player // Namespace for encapsulation and modularity
 {
-    public class PlayerController : MonoBehaviour, IPolarityObject
+    public class PlayerController : MonoBehaviour
     {
         public float jumpForce;
         public float moveSpeed = 5f;
         private Rigidbody2D rb;
         public bool isGrounded;
-        private bool polarity = true; // Default polarity
 
+        // private backing field for polarity
+        private bool polarity = true; 
+        // polarity is true = Ground is safe and Hazard is bad
+        // polarity is false = Ground is bad and Hazard is safe 
+
+        // Getter and Setter 
         public bool Polarity
         {
             get { return polarity; }
@@ -22,16 +27,21 @@ namespace Player
             rb = GetComponent<Rigidbody2D>();
         }
 
+        // Start is called before the first frame update
         void Start()
         {
+            //Run polarity check as a coroutine
             StartCoroutine(TogglePolarityCoroutine());
         }
 
+        // Update is called once per frame
         void Update()
         {
+            // Horizontal movement
             float moveInput = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+            // Jumping
             if (Input.GetButtonDown("Jump") && isGrounded)
             {
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
@@ -40,44 +50,31 @@ namespace Player
 
         private void OnCollisionEnter2D(Collision2D other)
         {
+            // SuperGround should always function normally, separate from polarity
             if(other.gameObject.CompareTag("SuperGround"))
             {
                 isGrounded = true;
             }
+            // Regular Collision with polarity switch
             if ((polarity && other.gameObject.CompareTag("Ground")) || (!polarity && other.gameObject.CompareTag("Hazard")))
             {
                 isGrounded = true;
             }
+            // Hazard Collision with polarity switch
             else if ((polarity && other.gameObject.CompareTag("Hazard")) || (!polarity && other.gameObject.CompareTag("Ground")))
             {
                 GameManager.Instance.GameOver();
-            }
-
-            // Semi-solid platform logic
-            if (other.gameObject.CompareTag("SemiSolid"))
-            {
-                if (rb.velocity.y < 0 && transform.position.y > other.transform.position.y)
-                {
-                    isGrounded = false;
-                }
-                else
-                {
-                    isGrounded = true;
-                }
             }
         }
 
         private void OnCollisionExit2D(Collision2D other)
         {
+            // Check collision exit with "Ground"
             if ((polarity && other.gameObject.CompareTag("Ground")) || (!polarity && other.gameObject.CompareTag("Hazard")))
             {
                 isGrounded = false;
             }
             if(other.gameObject.CompareTag("SuperGround"))
-            {
-                isGrounded = false;
-            }
-            if (other.gameObject.CompareTag("SemiSolid"))
             {
                 isGrounded = false;
             }
@@ -89,7 +86,10 @@ namespace Player
             {
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
-                    polarity = !polarity;
+                    // Toggle the polarity
+                    Polarity = !Polarity;
+
+                    // Debug log to indicate polarity switch
                     Debug.Log("Polarity switched: " + (polarity ? "Hazard -> Ground" : "Ground -> Hazard"));
                 }
 
